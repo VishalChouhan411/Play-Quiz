7fc8998be1391c68faf30ff17ed5d5b02443a0f9
VishalChouhan411 <145850156+VishalChouhan411@users.noreply.github.com>
2023-10-21 13:06:20 +0530

    Update Home.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,4 +1,7 @@
 import React from "react";
+import illus1 from "../images/quiz.png";
+import illus2 from "../images/3.jpg";
+import illus3 from "../images/2.jpg";
 import { HomeCard } from "./UI/HomeCard";
 
 
@@ -12,20 +15,25 @@ export const Home = () => {
         heading="Create New Quiz"
         path="create-new"
         delay={0.15}
-    
+        image={illus1}
+        size ={"500px"}
       />
       <HomeCard
         color="grey"
         heading="My Quizzes"
         path="my-quizzes"
         delay={0.25}
-       
+        image={illus3}
+        size ={"500px"}
       />
       <HomeCard
+        
         color="grey"
         heading="Play Quiz"
         path="play-quiz"
         delay={0.35}
+        image={illus2}
+        size ={"500px"}
         
       />
     </div>
